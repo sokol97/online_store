0058699f14987e1d08638c2dfcfdd73c73e7ad5f
sokol97 <48691564+sokol97@users.noreply.github.com>
2021-09-26 14:11:58 +0500

    Добавил модели для магазина

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -14,6 +15,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    class Meta:
+        abstract = True  # Не создает миграцию в БД, но может использоваться как родительский класс (Product)
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
     title = models.CharField(max_length=255, verbose_name="Наименование")
     slug = models.SlugField(unique=True)
@@ -24,11 +29,18 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+
+class NotebookProduct(Product):
+    pass
+
+
 class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name="Пользователь", on_delete=models.CASCADE)
-    cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name="Товар", on_delete=models.CASCADE)
+    cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE, related_name="related_products")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) # Получает все модели
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id") # Автоматически связывает
     qty = models.PositiveIntegerField(default=1)
     total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
 
@@ -37,7 +49,7 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
     owner = models.ForeignKey("Customer", verbose_name="Владелец", on_delete=models.CASCADE)
-    products =models.ManyToManyField(CartProduct, blank=True)
+    products =models.ManyToManyField(CartProduct, blank=True, related_name="related_cart")
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
 
@@ -53,13 +65,7 @@ class Customer(models.Model):
     def __str__(self):
         return f"окупатель {self.user.first_name} {self.user.last_name}"
 
-class Specifications(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name="Имя товара для характеристик")
 
-    def __str__(self):
-        return f"Характеристики для товара {self.name}"
 
 
 
