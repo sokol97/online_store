86658de06aa7f2555ea2d116e9b71438e3d7a533
sokol97 <48691564+sokol97@users.noreply.github.com>
2021-09-27 23:22:30 +0500

    Изменил и добавил модели Phone и Notebook

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -30,8 +30,31 @@ class Product(models.Model):
         return self.title
 
 
-class NotebookProduct(Product):
-    pass
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255, verbose_name="Диагональ")
+    display_type = models.CharField(max_length=255, verbose_name="Тип дисплея")
+    processor_freq = models.CharField(max_length=255, verbose_name="Частота процессора")
+    ram = models.CharField(max_length=255, verbose_name="Оперативная память")
+    video = models.CharField(max_length=255, verbose_name="Видеокарта")
+    time_without_charge = models.CharField(max_length=255, verbose_name="Время работы аккумулятора")
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
+
+
+class Phone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name="Диагональ")
+    display_type = models.CharField(max_length=255, verbose_name="Тип дисплея")
+    resolution = models.CharField(max_length=255, verbose_name="Разрешение экрана")
+    accum_value = models.CharField(max_length=255, verbose_name="Объем батареи")
+    ram = models.CharField(max_length=255, verbose_name="Оперативная память")
+    sd = models.BooleanField(default=True)
+    sd_value_max = models.CharField(max_length=255, verbose_name="Максимальный объем встраиваемой памяти")
+    main_cam_mp = models.CharField(max_length=255, verbose_name="Главная камера")
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name="Фронтальная камера")
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
 
 
 class CartProduct(models.Model):
