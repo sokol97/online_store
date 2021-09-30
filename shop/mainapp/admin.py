57623e31eb03e253ac204565660559dc6806e763
sokol97 <48691564+sokol97@users.noreply.github.com>
2021-09-30 21:19:10 +0500

    Добавил проверку изображения на расширение

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,16 +1,36 @@
 from django.contrib import admin
 from .models import *
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+from PIL import Image
 
+
+class CheckImage(ModelForm):
+
+    MIN_RESOLUTIONS = (250, 250)
+
+    # Добавляет подсказку(надпись) к указанному полю fields[] в админке
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = f"Загружайте изображение с" \
+                                         f" минимальным разрешением {self.MIN_RESOLUTIONS[0]} x {self.MIN_RESOLUTIONS[1]}"
+        self.fields['price'].help_text = 'Укажите цену в долларах'
+
+    # Проверка на расширение изображения
+    def clean_image(self):
+        image = self.cleaned_data['image'] # cleaned_data возвращает словарь всех
+                                        # проверенных полей, мы запршивааем поле image
+        img = Image.open(image)
+        if img.height < self.MIN_RESOLUTIONS[0] or img.width < self.MIN_RESOLUTIONS[1]:
+            raise ValidationError("Разрешение изображения меньше минимального!")
+        return image
 
 """Конструкция для ограничения категорий для товара"""
 # Для ноутбуков
 
-
-
-# @admin.register(Notebook)
 class NotebookAdmin(admin.ModelAdmin):
+
     list_display = ('title', 'image', 'diagonal', 'display_type', 'processor_freq', 'ram', 'video', 'time_without_charge')
+    form = CheckImage
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
@@ -21,11 +41,10 @@ class NotebookAdmin(admin.ModelAdmin):
 
 # Для телефонов
 
-
-
-# @admin.register(Phone)
 class PhoneAdmin(admin.ModelAdmin):
+
     list_display = ('title', 'image', 'diagonal', 'display_type', 'resolution', 'ram', 'accum_value', 'sd', 'sd_value_max', 'main_cam_mp', 'frontal_cam_mp')
+    form = CheckImage
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
